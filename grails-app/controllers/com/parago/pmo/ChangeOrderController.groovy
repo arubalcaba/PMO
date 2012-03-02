a276a180a315914b1975007dbfd0728ac3f5b0fb
Andrew Rubalcaba <a.rubalcaba@gmail.com>
2012-03-02 15:04:25 -0600

    changes to Change Order Controller

--- grails-app/controllers/com/parago/pmo/ChangeOrderController.groovy
+++ grails-app/controllers/com/parago/pmo/ChangeOrderController.groovy
@@ -1,6 +1,7 @@
 package com.parago.pmo
 
 import org.springframework.dao.DataIntegrityViolationException
+import grails.converters.JSON
 
 class ChangeOrderController {
 
@@ -27,6 +28,9 @@ class ChangeOrderController {
         }
 
 		flash.message = message(code: 'default.created.message', args: [message(code: 'changeOrder.label', default: 'ChangeOrder'), changeOrderInstance.id])
+		withFormat{
+			
+		}
         redirect(action: "show", id: changeOrderInstance.id)
     }
 
@@ -89,27 +93,99 @@ class ChangeOrderController {
 					response.status = 405;
 					render "Change Order ${params.id} not found"
 				}
+				
+				if (params.version) {
+					def version = params.version.toLong()
+					if (changeOrderInstance.version > version) {
+						changeOrderInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+								  [message(code: 'changeOrder.label', default: 'ChangeOrder')] as Object[],
+								  "Another user has updated this ChangeOrder while you were editing")
+						render changeOrderInstance.errors.allErrors as JSON;
+					}
+				}
+				
+				def columnId = params.columnId;
+				if(columnId == 2)
+					changeOrderInstance.changeOrderName = params.value;
+				else
+					changeOrderInstance.link = params.value;
+				
+					if (!changeOrderInstance.save(flush: true)) {
+						response.status = 404;
+						message = "an error occurred";
+						render message as JSON;
+					}
+				
+				response.status = 200;
+				render text:params.value
 			}	
 		}
        
     }
 
     def delete() {
-        def changeOrderInstance = ChangeOrder.get(params.id)
-        if (!changeOrderInstance) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: 'changeOrder.label', default: 'ChangeOrder'), params.id])
-            redirect(action: "list")
-            return
-        }
-
-        try {
-            changeOrderInstance.delete(flush: true)
-			flash.message = message(code: 'default.deleted.message', args: [message(code: 'changeOrder.label', default: 'ChangeOrder'), params.id])
-            redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
-			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'changeOrder.label', default: 'ChangeOrder'), params.id])
-            redirect(action: "show", id: params.id)
-        }
+		withFormat{
+			html{
+				def changeOrderInstance = ChangeOrder.get(params.id)
+				if (!changeOrderInstance) {
+					flash.message = message(code: 'default.not.found.message', args: [message(code: 'changeOrder.label', default: 'ChangeOrder'), params.id])
+					redirect(action: "list")
+					return
+				}
+		
+				try {
+					changeOrderInstance.delete(flush: true)
+					flash.message = message(code: 'default.deleted.message', args: [message(code: 'changeOrder.label', default: 'ChangeOrder'), params.id])
+					redirect(action: "list")
+				}
+				catch (DataIntegrityViolationException e) {
+					flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'changeOrder.label', default: 'ChangeOrder'), params.id])
+					redirect(action: "show", id: params.id)
+				}
+				
+			}
+			json{
+				def changeOrderInstance = ChangeOrder.get(params.id)
+				if (!changeOrderInstance) {
+					response.status = 405;
+					render "Change Order ${params.id} not found"
+				}
+				try
+				{
+					changeOrderInstance.delete(flush:true);
+					response.status = 200;
+					render text:"ok";
+				}
+				catch (DataIntegrityViolationException e) {
+					message = message(code: 'default.not.deleted.message', args: [message(code: 'changeOrder.label', default: 'ChangeOrder'), params.id])
+					response.status = 405;
+					render message as JSON;
+				}
+				
+			}
+		}
+       
     }
+	
+	def dataTablesSource(){
+		def projectInfo = ProjectInfo.get(params.projectInfoId);
+		def changeOrderList = projectInfo ? ChangeOrder.findAllByProjectInfo(projectInfo):[];
+		def jsonResponse = [:];
+		jsonResponse.aaData=[];
+		
+		changeOrderList?.each{ changeOrder -> 
+			jsonResponse.aaData << [changeOrder.id,
+									changeOrder.projectInfo.id,
+									changeOrder.changeOrderName,
+									changeOrder.link]
+		}
+		
+		render jsonResponse as JSON;
+		
+		
+		
+		
+		
+		
+	}
 }
